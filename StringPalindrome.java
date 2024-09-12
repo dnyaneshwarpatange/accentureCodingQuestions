e26fab28e6243e1e7575e6a3efec43f7b32eddba
Dnyaneshwar Patange <patangednyaneshwar7@gmail.com>
2024-09-12 11:39:58 +0530

    String Palindrome

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,13 +1,13 @@
 public class StringPalindrome{
     public static void main(String[] args) {
-        String inpuString = "hello";
+        String inpuString = "lol";
         System.out.println(checkPalindrome(inpuString));
         
     }
     public static int checkPalindrome(String inputString){
         String string1 = new StringBuilder(inputString).reverse().toString();
         String string2 = inputString;
-        if (string1 == string2) {
+        if (string1.equals(string2)) {
             System.out.println("String Palindrome");
             
         }else{
